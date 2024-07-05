07f07c7b0bff1431d4bba11940e38fdfdff4bac8
Fridhi-Rochdi <rochdifridhi39@gmail.com>
2024-07-05 22:15:25 +0100

    first commit

--- script.js
+++ script.js
@@ -10,3 +10,41 @@ $(document).ready(function() {
         }
     });
 });
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const sectionId = this.getAttribute('href');
+        document.querySelector(sectionId).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+window.addEventListener('scroll', function() {
+    const section = document.getElementById('section-id'); // Remplacez par l'ID de votre section
+    if (isInViewPort(section)) {
+       
+        section.classList.add('loaded');
+    }
+});
+
+function isInViewPort(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+const sponsorLogos = document.querySelectorAll('.sponsor-logos img');
+sponsorLogos.forEach(logo => {
+    logo.addEventListener('mouseenter', function() {
+        this.style.transform = 'scale(1.1)';
+    });
+    logo.addEventListener('mouseleave', function() {
+        this.style.transform = 'scale(1)';
+    });
+});
+
